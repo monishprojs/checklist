d67b00001c63c5d4f7a17c0b50f5dae05dc10f6a
Monish <monishkumar833@gmail.com>
2022-08-07 16:14:02 -0400

    works with enter key now as well

--- src/components/checkList.tsx
+++ src/components/checkList.tsx
@@ -3,7 +3,15 @@ import './checkList.css';
 
 
 function CheckList() {
-    const [checkList, setList] = useState(['Start of Checklist']);
+    const [checkList, setList] = useState(['']);
+    const [dates, setDates] = useState([''])
+
+    function enter(key: any) {
+        if (key.key === 'Enter') {
+            addList();
+        }
+    }
+
     function addList() {
         let addBox = document.getElementById('add') as HTMLInputElement | null;
         if (addBox != null) {
@@ -14,10 +22,12 @@ function CheckList() {
     return (
         <div>
             <div className="functions">
-                <input type="text" id="add" className="subject" />
+                <input type="text" id="add" className="subject" onKeyDown={enter} />
+                &nbsp;
                 <button onClick={addList} className="submit">Submit</button>
             </div>
             <div className="list">
+                <p>Items:</p>
                 {checkList.map(check => {
                     return (
                         <div>
