f854e7e72f2691a2244e3a0a847fed7758d558f8
Monish <monishkumar833@gmail.com>
2022-08-07 13:26:47 -0400

    added update function

--- src/components/checkList.tsx
+++ src/components/checkList.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
+import { createDocumentRegistry } from 'typescript';
 import './checkList.css';
 
 
 function CheckList() {
     const [checkList, setList] = useState(['Start of Checklist']);
+    function addList(item: string) {
+        setList(checkList => [...checkList, item]);
+    }
     return (
         <div className="list">
             {checkList.map(check => {
